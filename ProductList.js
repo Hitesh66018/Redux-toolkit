f240d793ff8d75c5245ed199c1c7392b7fadebb4
Hitesh Sharma <hiteshsharma66018@gmail.com>
2025-10-29 15:40:09 +0530

    Create ProductList.js

---
+++ ProductList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../features/cart/cartSlice';
+
+const products = [
+  { id: 1, name: 'Laptop', price: 1200 },
+  { id: 2, name: 'Smartphone', price: 800 },
+  { id: 3, name: 'Headphones', price: 150 }
+];
+
+const ProductList = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>Products</h2>
+      {products.map(product => (
+        <div key={product.id}>
+          <h3>{product.name}</h3>
+          <p>Price: ${product.price}</p>
+          <button onClick={() => dispatch(addToCart(product))}>Add to Cart</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
